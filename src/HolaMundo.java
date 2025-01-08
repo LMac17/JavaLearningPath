cee4d3aa0c79689a5d940fd2093fe406a77872b1
LuisMac <luismacod5@gmail.com>
2025-01-08 10:08:20 -0600

    HolaMundo file added and gitignore file modified

---
+++ src/HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+    }
+    /* Para identar código en IntelliJ:
+     * Windows: Ctrl + Alt + L
+     * Linux: Ctrl + Windows Key + Alt + L
+     * MacOS: CMD + Option + L
+     */
+}
